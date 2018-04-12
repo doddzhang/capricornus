4c0d1e68abfb727a4c2083e9e33eb22d8c4d5f24
zhangdong <zhangdong@chinadaas.com>
2018-04-12 14:47:25 +0800

    config document

--- config-client/src/main/java/com/doddzhang/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/doddzhang/configclient/ConfigClientApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ *
+ * /{application}/{profile}[/{label}]
+ /{application}-{profile}.yml
+ /{label}/{application}-{profile}.yml
+ /{application}-{profile}.properties
+ /{label}/{application}-{profile}.properties
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigClientApplication {
